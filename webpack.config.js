59985188a012c8546673d8f4f4a1077c3146c239
Simbarashe Mupfukudzwa <simba3310@gmail.com>
2018-04-01 13:04:15 -0500

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/app",
+  output: {
+    path: path.join(__dirname, "/dist"),
+    filename: bundle.js
+  },
+  module: {
+    rules: [
+      {
+        use: {
+          loader: "babel-loader"
+        },
+        test: /\.js$/,
+        exclude: node_modules
+      }
+    ]
+  },
+  plugins:[
+    new HtmlWebpackPlugin({
+      template:'./src/index'
+    })
+  ]
+
+};
